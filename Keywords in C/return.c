8d1f5bd97c71055fddb15caddb16e85142042586
prasannabarge <prasannabarge08@gmail.com>
2024-07-31 15:31:30 +0530

    C keyword

---
+++ Keywords in C/return.c
@@ -0,0 +1,17 @@
+// C program to demonstrate
+// return keyword
+#include <stdio.h>
+int sum(int x, int y) {
+  int sum;
+  sum = x + y;
+  return sum;
+}
+
+// Driver code
+int main() {
+  int num1 = 10;
+  int num2 = 20;
+  printf("Sum: %d", 
+          sum(num1, num2));
+  return 0;
+}
