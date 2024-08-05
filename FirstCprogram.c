0bc4b753a2783209a8af76e00397820d040758c4
prasannabarge <prasannabarge08@gmail.com>
2024-08-05 15:13:54 +0530

    First C Program

---
+++ FirstCprogram.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+  int a  =10;
+  printf("%d",a);
+  return 0;
+
+}
+
+/*you can execute the command by these also:
+ 1.cmd:
+ gcc FirstCprogram.c -o FirstCprogram
+ FirstCprogram.exe or FirstCprogram
+
+ or
+
+ gcc FirstCprogram.c
+ a.exe
+
+  
+
+ 2.git bash:
+ gcc FirstCprogram.c -o FirstCprogram
+ ./FirstCprogram
+*/
+
+
+//by this command you can see all  generated intermediate files:
+ //$gcc -Wall -save-temps FirstCprogram.c –o FirstCprogram
