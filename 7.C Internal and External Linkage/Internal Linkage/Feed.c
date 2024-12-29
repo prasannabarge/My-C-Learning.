2b085170f57bf1af1be5f5511a01e765d33217dd
prasannabarge <prasannabarge08@gmail.com>
2024-12-29 19:21:32 +0530

    C Internal and External Linkage

--- 7.C Internal and External Linkage/Internal Linkage/Feed.c
+++ 7.C Internal and External Linkage/Internal Linkage/Feed.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
-<<<<<<< HEAD
-#include "Animals.c"
+#include "animals.c"
 
-int main(){
-  //Accesing variables 
-=======
-
-
-
-int main(void){
-  call_me();
-   int animals = 2;
->>>>>>> 8146c09d1a8e24f6e45f2742940f5d9c1cfcec27
-  printf("%d",animals);
-  return 0;
+int main() {
+  
+      // Accessing variable.
+    printf("%d", animals);
+    return 0;
 }
