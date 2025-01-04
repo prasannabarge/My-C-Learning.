4a62a51d1cdd9a1616fb3e37cefd0b403a8cd5fd
prasannabarge <prasannabarge08@gmail.com>
2025-01-04 10:57:56 +0530

    C Literals

--- 10.C Literals/Integer Literals.c
+++ 10.C Literals/Integer Literals.c
@@ -9,3 +9,9 @@ int main(void){
   printf("Integer Literal: %d\n", intVal);
   return 0;
 }
+// int: No suffix is required because integer constant is by default assigned as an int data type.
+// unsigned int: character u or U at the end of an integer constant.
+// long int: character l or L at the end of an integer constant.
+// unsigned long int: character ul or UL at the end of an integer constant.
+// long long int: character ll or LL at the end of an integer constant.
+// unsigned long long int: character ull or ULL at the end of an integer constant.
